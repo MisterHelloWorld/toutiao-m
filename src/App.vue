d45ba9ede040e390932d4c9662ddde1ea128d6a5
xiezhiqiang <aigirl8058@icloud.com>
2022-04-19 11:34:43 +0800

    项目初始化-引入Vant组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>新闻头条</div>
+  <div>
+    新闻头条
+    <span class="toutiao toutiao-sousuo"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'
+// 引入Vant组件库
+import Vant from 'vant'
+// 引入Vant全局样式
+import 'vant/lib/index.css'
+// 全局注册Vant组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
